57963119b1fb2dde65055435f010744a5a522850
Ryan Fang <41391053+R4BBT@users.noreply.github.com>
2020-12-08 19:59:32 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 # sudo apt-get update
 # sudo apt-get upgrade
 sudo apt-get install git wget -yq
+sudo apt install openjdk-8-jdk -yq
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
