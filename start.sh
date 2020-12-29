b217c250e1ba78097886d7491d2f491c92e0dc55
R4BBT <ryanfangk@gmail.com>
2020-12-29 13:04:48 +0000

    12/29/2020

--- start.sh
+++ start.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# sudo apt-get update
-# sudo apt-get upgrade
+# This script works best with Ubuntu 16.04 LTS
 sudo apt-get install git wget -yq
 sudo apt-get install openjdk-8-jdk -yq
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
